77702c3f98e4a44c00a4619425d7f6b5adf54422
Juha <kukiyomenaisapp@gmail.com>
2023-01-11 22:51:13 +0900

    コメントを消した

--- FireDeerTest/Program.cs
+++ FireDeerTest/Program.cs
@@ -7,21 +7,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        /*
-        FireeeeDeeeer fireeeeDeeeer = new FireeeeDeeeer(args, new Command[] {
-            new Command(
-                new Require[]{
-                    new StringArgumentRequire()
-                },
-                args => {
-                    foreach (var arg in args)
-                    {
-                        Console.WriteLine(arg.GetType().Name);
-                    }
-                }
-            )
-        });*/
-
         FireeeeDeeeer fireeeeDeeeer = new FireeeeDeeeer(args,
             new CommandBuilder()
             .AddRequire(new StringArgumentRequire())
