0afacca940031d85ed5bc5253d127baefb61b99a
Juha <kukiyomenaisapp@gmail.com>
2023-01-11 22:11:41 +0900

    RequireにTryParseを用意した

--- FireDeer/Arguments/StringArgument.cs
+++ FireDeer/Arguments/StringArgument.cs
@@ -8,14 +8,4 @@ public class StringArgument : IArgument
     {
         this.literal = literal;
     }
-
-    public static IArgument? Parse(Queue<string> rawArgs)
-    {
-        if (rawArgs.TryDequeue(out string? rawArg) && rawArg != null)
-        {
-            return new StringArgument(rawArg);
-        }
-
-        return null;
-    }
 }
--- FireDeer/FireeeeDeeeer.cs
+++ FireDeer/FireeeeDeeeer.cs
@@ -6,10 +6,6 @@ public class FireeeeDeeeer
 {
     string[] rawArgs;
     List<Command> commands;
-    List<Func<Queue<string>, IArgument?>> argumentParseFunctions = new List<Func<Queue<string>, IArgument?>>()
-    {
-        StringArgument.Parse
-    };
 
     public FireeeeDeeeer(string[] rawArgs, Command[] commands)
     {
@@ -19,25 +15,6 @@ public class FireeeeDeeeer
 
     public IArgument[] Parse()
     {
-        List<IArgument> ret = new List<IArgument>(rawArgs.Length);
-        Queue<string> rawArgsQueue = new Queue<string>(rawArgs);
 
-        while (rawArgsQueue.TryPeek(out string? result))
-        {
-            IArgument? parsedArg = ParseArgument(rawArgsQueue);
-            if (parsedArg != null) ret.Add(parsedArg);
-        }
-
-        return ret.ToArray();
-    }
-
-    IArgument? ParseArgument(Queue<string> rawArgsQueue)
-    {
-        foreach (var argumentParseFunction in argumentParseFunctions)
-        {
-            return argumentParseFunction(rawArgsQueue);
-        }
-
-        return null;
     }
 }
