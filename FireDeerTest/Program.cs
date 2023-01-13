3452649dd3861440fea1395ccb37d53429ad18dd
KYapp <kukiyomenaisapp@gmail.com>
2023-01-13 17:05:45 +0900

    バルスッッ!!!!

--- FireDeerTest/Program.cs
+++ FireDeerTest/Program.cs
@@ -1,22 +1,9 @@
-﻿using FireDeer;
-using FireDeer.Requiries;
-
-namespace FireDeerTest;
+﻿namespace FireDeerTest;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        FireeeeDeeeer fireeeeDeeeer = new FireeeeDeeeer(args, "firedeerのテスト用コマンド", true,
-            new CommandBuilder()
-                .AddRequire(new StringArgumentRequire())
-                .AddRequire(new IdentifierArgumentRequire("foo"))
-                .AddRequire(new IntegerArgumentRequire())
-                .SetAction(args => { foreach (var arg in args) Console.WriteLine(arg.GetType().Name); })
-                .Build()
-        );
 
-        if (fireeeeDeeeer.Run()) Console.WriteLine("成功");
-        else Console.WriteLine("失敗");
     }
 }
